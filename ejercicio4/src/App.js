4db0cce32164707b917cca7946852785ad47125d
kevCamarg0 <kcamargo673@gmail.com>
2023-04-18 18:52:44 -0700

    4

--- ejercicio4/src/App.js
+++ ejercicio4/src/App.js
@@ -1,14 +1,20 @@
 import './App.css';
 import Inicio from './Inicio';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import ComponenteDemo from './ComponenteDemo';
+import About from './About';
+import Contact from './Contact';
+import FAQ from './FAQ';
+import Pricing from './Pricing';
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path='/' element={<Inicio/>}/>
-        <Route path='/demo' element={<ComponenteDemo/>}/>
+        <Route path='/about' element={<About/>}/>
+        <Route path='/contact' element={<Contact/>}/>
+        <Route path='/pricing' element={<Pricing/>}/>
+        <Route path='/faq' element={<FAQ/>}/>
       </Routes>
     </BrowserRouter>
   );
